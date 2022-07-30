7a0a14ba8c472705d89fa0fe764c28e8aa66e70b
Crowdbotics <team@crowdbotics.com>
2022-07-30 02:07:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "extreme_inflatables_36498",
+  url: "https://extreme_inflatables_36498.botics.co",
+  api: "https://extreme_inflatables_36498.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
